0e564a13b9209ac6a1c5d4c2dd0ebcfa65f20cc0
Sittiwerapong <JSittiwerapong@dstworldwideservices.com>
2017-02-24 11:39:52 +0700

    init

--- JAXBParser/src/main/java/com/example/util/JAXBObjectParserManager.java
+++ JAXBParser/src/main/java/com/example/util/JAXBObjectParserManager.java
@@ -1,23 +1,3 @@
-/**
- * Copyright (c) DST Output, LLC, 2005-2013
- * All rights reserved.
- * 
- * Unpublished rights reserved under the copyright laws of the United States. The
- * Software contained on this media is proprietary to and embodies the confidential
- * technology of DST Output, LLC.  Possession, use, duplication or dissemination of 
- * the software and media is authorized only pursuant to a valid written license 
- * from DST Output, LLC.
- *
- * RESTRICTED RIGHTS LEGEND
- * Use, duplication, or disclosure by the U.S. Government is subject to restrictions
- * as set forth in Subparagraph (c) (1) (ii) of DFARS 252.227-7013 or in FAR
- * 52.227-19, as applicable.
- * 
- * $Header: //DICE/batch/GenericMsgListener/Main/GenericMsgListener/src/com/dsto/dice/gml/util/JAXBObjectParserManagers.java#2 $
- * $Author: dt85715 $
- * $Change: 212010 $
- * $DateTime: 
- */
 package com.example.util;
 
 import java.lang.reflect.Method;
